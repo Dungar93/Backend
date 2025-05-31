6e1ada69faae583f0c60095138537fb08b52fd10
Dungar93 <b23es1015@iitj.ac.in>
2025-06-01 00:11:37 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -15,7 +15,16 @@ dotenv.config ({
 
 
  connectDB()
-
+.then(()=>{
+    app.listen (process.env.PORT || 8000,() =>{
+        console.log(`server is running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) =>{
+    console.log("Mongodb connection is failed ");
+    
+})
 
 /*
 import express from "express"
---
+++ src/utils/ApiError.js
@@ -0,0 +1,28 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message  = "Something went wrong",
+        errors  = [],
+        statck  = ""
+
+    ){
+        super(message)
+            this.statusCode =  statusCode
+            this.message =  message
+            this.error  =  error 
+            this.data  =  null
+            this.success  =  false
+            this.errors  =  errors 
+            if (statck){
+                this.stack = statck
+
+            }
+            else {
+                Error.captureStackTrace(this, this.constructer)
+            }
+
+
+        
+    }
+}
+export {ApiError}
