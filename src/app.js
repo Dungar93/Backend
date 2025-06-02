6db05fc2a0bd711b61cfa51ade3a5a1f85f2c8bc
Dungar93 <dungarsoni1@gmail.com>
2025-06-02 13:42:16 +0500

    all setup configured

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ app.use (cors({
 app.use (express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended : true,limit :"16kb"}))
 app.use (express.static("public"))
-app.use (cookieParser)
+app.use (cookieParser())
+// routes import 
+import userRouter from './routes/user.js'
 
+// routes declarations 
+
+app.use ("/api/v1/users",userRouter)
 export {app}
